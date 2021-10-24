bc66133d22347d35f08be3214f0e5dbf03286ca6
gesse-fernandes <gesse-fm@hotmail.com>
2021-10-23 23:36:51 -0300

    um pouco sobre variavel bem similar ao java, javascript e c# e na linguagem C

---
+++ main.dart
@@ -0,0 +1,17 @@
+main(List<String> args) {
+  String name = "gesse";
+  int num = 1;
+  double num2 = 2.5;
+  print("Hello World! ${name}" + " ${num}");
+  print("${num2}");
+
+  var text = "meu primeiro objeto dinamico";
+
+  print(text);
+
+  dynamic text3 = "oi teste";
+  print(text3);
+
+  const text2 = "kkk";
+  print(text2);
+}
